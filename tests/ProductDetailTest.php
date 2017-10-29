24f44e2e5d4ea0b0a3a674f3a1bada4577b4b65e
Ondřej Machulda <ondrej.machulda@gmail.com>
2017-10-29 20:12:28 +0100

    Implement basic test

--- tests/ProductDetailTest.php
+++ tests/ProductDetailTest.php
@@ -2,6 +2,8 @@
 
 namespace My;
 
+use Facebook\WebDriver\WebDriverBy;
+
 class ProductDetailTest extends AbstractTestCase
 {
     /**
@@ -11,6 +13,18 @@ class ProductDetailTest extends AbstractTestCase
     {
         $this->wd->get(static::getBaseUrl() . 'products/t-shirt-minus');
 
-        sleep(5);
+        $header = $this->wd->findElement(WebDriverBy::cssSelector('h1'))
+            ->getText();
+        // Or use syntax sugar: $header = $this->findByCss('h1')->getText();
+        $this->assertSame('T-Shirt "minus"', $header);
+
+        $this->debug('Product detail header was: ' . $header);
+
+        $price = $this->wd->findElement(WebDriverBy::cssSelector('#product-price'))
+            ->getText();
+        // Or use syntax sugar: $price = $this->findByCss('#product-price')->getText();
+        $this->assertSame('€1.48', $price);
+
+        $this->debug('Product detail header was: ' . $price);
     }
 }
